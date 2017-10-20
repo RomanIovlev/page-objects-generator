76f3b124082e5937507e18b1bcb4e6a1b92bcff4
pochanin <roma_02.97@mail.ru>
2017-10-20 12:03:38 +0300

    Complex elements can be built

--- src/main/java/com/epam/page/object/generator/builder/CommonFieldsBuilder.java
+++ src/main/java/com/epam/page/object/generator/builder/CommonFieldsBuilder.java
@@ -3,11 +3,20 @@ package com.epam.page.object.generator.builder;
 import static com.epam.page.object.generator.builder.StringUtils.splitCamelCase;
 import static com.squareup.javapoet.AnnotationSpec.builder;
 
+import com.epam.jdi.uitests.web.selenium.elements.pageobjects.annotations.objects.JComboBox;
+import com.epam.jdi.uitests.web.selenium.elements.pageobjects.annotations.objects.JDropList;
+import com.epam.jdi.uitests.web.selenium.elements.pageobjects.annotations.objects.JDropdown;
+import com.epam.jdi.uitests.web.selenium.elements.pageobjects.annotations.objects.JMenu;
+import com.epam.jdi.uitests.web.selenium.elements.pageobjects.annotations.objects.JTable;
+import com.epam.page.object.generator.model.ElementAttribute;
 import com.epam.page.object.generator.model.SearchRule;
+import com.squareup.javapoet.AnnotationSpec;
 import com.squareup.javapoet.FieldSpec;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.lang.model.element.Modifier;
 import org.openqa.selenium.support.FindBy;
 
@@ -15,24 +24,101 @@ public class CommonFieldsBuilder implements IFieldsBuilder {
 
     public Class elementClass;
 
+    Map<String, Class> annotationMap = new HashMap<>();
+
+    {
+        annotationMap.put("button", FindBy.class);
+        annotationMap.put("checkbox", FindBy.class);
+        annotationMap.put("datepicker", FindBy.class);
+        annotationMap.put("fileinput", FindBy.class);
+        annotationMap.put("image", FindBy.class);
+        annotationMap.put("input", FindBy.class);
+        annotationMap.put("label", FindBy.class);
+        annotationMap.put("link", FindBy.class);
+        annotationMap.put("text", FindBy.class);
+        annotationMap.put("textarea", FindBy.class);
+        annotationMap.put("textfield", FindBy.class);
+
+        annotationMap.put("menu", JMenu.class);
+        annotationMap.put("radiobuttons", FindBy.class);
+        annotationMap.put("selector", FindBy.class);
+        annotationMap.put("tabs", FindBy.class);
+        annotationMap.put("textlist", FindBy.class);
+        annotationMap.put("table", JTable.class);
+        annotationMap.put("checklist", FindBy.class);
+        annotationMap.put("combobox", JComboBox.class);
+        annotationMap.put("dropdown", JDropdown.class);
+        annotationMap.put("droplist", JDropList.class);
+        annotationMap.put("elements", FindBy.class);
+    }
+
     public CommonFieldsBuilder(Class elementClass) {
         this.elementClass = elementClass;
     }
 
     public List<FieldSpec> buildField(SearchRule searchRule, String url) throws IOException {
         List<FieldSpec> abstractFields = new ArrayList<>();
-        List<String> elements = searchRule.getElements(url);
+        List<String> elementsRequiredValues = searchRule
+            .extractRequiredValuesFromFoundElements(url);
 
-        for (String element : elements) {
-            abstractFields.add(FieldSpec.builder(elementClass, splitCamelCase(element))
-                .addModifiers(Modifier.PUBLIC)
-                .addAnnotation(builder(FindBy.class)
-                    .addMember("xpath", "$S", createXPathSelector(searchRule, element))
-                    .build())
-                .build());
+        for (String elementsRequiredValue : elementsRequiredValues) {
+
+            FieldSpec.Builder field = FieldSpec
+                .builder(elementClass, splitCamelCase(elementsRequiredValue))
+                .addModifiers(Modifier.PUBLIC);
+
+            field.addAnnotation(buildAnnotation(searchRule, elementsRequiredValue));
+
+            abstractFields.add(field.build());
         }
 
         return abstractFields;
+    }
+
+    private AnnotationSpec buildAnnotation(SearchRule searchRule, String elementsRequiredValue) {
+        if (searchRule.innerSearchRules == null) {
+            return buildCommonAnnotation(searchRule, elementsRequiredValue);
+        } else {
+            return buildComplexAnnotation(searchRule);
+        }
+    }
+
+    private AnnotationSpec buildCommonAnnotation(SearchRule searchRule,
+        String elementsRequiredValue) {
+        return builder(annotationMap.get(searchRule.type))
+            .addMember("xpath", "$S", createXPathSelector(searchRule, elementsRequiredValue))
+            .build();
+    }
+
+    private AnnotationSpec buildComplexAnnotation(SearchRule searchRule) {
+        AnnotationSpec.Builder annotationBuilder = AnnotationSpec
+            .builder(annotationMap.get(searchRule.type));
+
+        for (SearchRule innerSearchRule : searchRule.innerSearchRules) {
+            AnnotationSpec.Builder innerAnnotation = AnnotationSpec.builder(FindBy.class);
+
+            if (innerSearchRule.tag != null) {
+                innerAnnotation.addMember("tagName", "$S", innerSearchRule.tag);
+                annotationBuilder
+                    .addMember(innerSearchRule.requiredAttribute, "$L", innerAnnotation.build());
+            }
+
+            for (String clazz : innerSearchRule.classes) {
+                innerAnnotation
+                    .addMember("class", "$S", clazz);
+                annotationBuilder
+                    .addMember(innerSearchRule.requiredAttribute, "$L", innerAnnotation.build());
+            }
+
+            for (ElementAttribute attribute : innerSearchRule.attributes) {
+                innerAnnotation
+                    .addMember(attribute.getAttributeName(), "$S", attribute.getAttributeValue());
+                annotationBuilder
+                    .addMember(innerSearchRule.requiredAttribute, "$L", innerAnnotation.build());
+            }
+        }
+
+        return annotationBuilder.build();
     }
 
     private String createXPathSelector(SearchRule searchRule, String element) {
