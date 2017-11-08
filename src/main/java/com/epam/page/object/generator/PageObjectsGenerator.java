a5873a7f5240c78910437201614ffdfd06e93e3e
Walrus94 <arsnazarov94@gmail.com>
2017-11-08 18:03:06 +0300

    EPMLSTRTAI-309: refactored according tai codestyle

--- src/main/java/com/epam/page/object/generator/PageObjectsGenerator.java
+++ src/main/java/com/epam/page/object/generator/PageObjectsGenerator.java
@@ -15,57 +15,58 @@ import java.util.List;
 
 public class PageObjectsGenerator {
 
-	private JsonRuleMapper parser;
-	private JavaFileWriter fileWriter;
-	private PageFieldSpecBuilder pageFieldSpecBuilder;
-	private SiteFieldSpecBuilder siteFieldSpecBuilder;
-	private SearchRuleValidator validator;
-	private List<String> urls;
-	private String packageName;
+    private JsonRuleMapper parser;
+    private JavaFileWriter fileWriter;
+    private PageFieldSpecBuilder pageFieldSpecBuilder;
+    private SiteFieldSpecBuilder siteFieldSpecBuilder;
+    private SearchRuleValidator validator;
+    private List<String> urls;
+    private String packageName;
 
-	private boolean forceGenerateFile = false;
+    private boolean forceGenerateFile = false;
 
-	public PageObjectsGenerator(JsonRuleMapper parser,
-	                            JavaFileWriter fileWriter,
-								PageFieldSpecBuilder pageFieldSpecBuilder,
-	                            SiteFieldSpecBuilder siteFieldSpecBuilder,
-	                            SearchRuleValidator validator,
-	                            List<String> urls,
-	                            String packageName) {
-		this.parser = parser;
-		this.fileWriter = fileWriter;
-		this.pageFieldSpecBuilder = pageFieldSpecBuilder;
-		this.siteFieldSpecBuilder = siteFieldSpecBuilder;
-		this.validator = validator;
-		this.urls = urls;
-		this.packageName = packageName;
-	}
+    public PageObjectsGenerator(JsonRuleMapper parser,
+                                JavaFileWriter fileWriter,
+                                PageFieldSpecBuilder pageFieldSpecBuilder,
+                                SiteFieldSpecBuilder siteFieldSpecBuilder,
+                                SearchRuleValidator validator,
+                                List<String> urls,
+                                String packageName) {
+        this.parser = parser;
+        this.fileWriter = fileWriter;
+        this.pageFieldSpecBuilder = pageFieldSpecBuilder;
+        this.siteFieldSpecBuilder = siteFieldSpecBuilder;
+        this.validator = validator;
+        this.urls = urls;
+        this.packageName = packageName;
+    }
 
-	public void generatePageObjects() throws IOException, URISyntaxException {
-		List<SearchRule> searchRules = parser.getRulesFromJSON();
+    public void generatePageObjects() throws IOException, URISyntaxException {
+        List<SearchRule> searchRules = parser.getRulesFromJSON();
 
-		try {
-			validator.validate();
-		} catch (ValidationException | NotUniqueSelectorsException ex) {
-			if (forceGenerateFile) {
-				generateJavaFiles(searchRules);
-			}
-			throw ex;
-		}
+        try {
+            validator.validate();
+        } catch (ValidationException | NotUniqueSelectorsException ex) {
+            if (forceGenerateFile) {
+                generateJavaFiles(searchRules);
+            }
+            throw ex;
+        }
 
-		generateJavaFiles(searchRules);
-	}
+        generateJavaFiles(searchRules);
+    }
 
-	private void generateJavaFiles(List<SearchRule> searchRules) throws IOException, URISyntaxException {
-		for (String url: urls) {
-			fileWriter.write(packageName + ".page", pageFieldSpecBuilder.build(searchRules, url));
-		}
+    private void generateJavaFiles(List<SearchRule> searchRules)
+        throws IOException, URISyntaxException {
+        for (String url : urls) {
+            fileWriter.write(packageName + ".page", pageFieldSpecBuilder.build(searchRules, url));
+        }
 
-		fileWriter.write(packageName + ".site", siteFieldSpecBuilder.build(urls));
-	}
+        fileWriter.write(packageName + ".site", siteFieldSpecBuilder.build(urls));
+    }
 
-	public void setForceGenerateFile(boolean forceGenerateFile) {
-		this.forceGenerateFile = forceGenerateFile;
-	}
+    public void setForceGenerateFile(boolean forceGenerateFile) {
+        this.forceGenerateFile = forceGenerateFile;
+    }
 
 }
