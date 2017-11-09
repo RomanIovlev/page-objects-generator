f02b9bdbecf7139770f7398f4b077a816d05c3eb
Walrus94 <arsnazarov94@gmail.com>
2017-11-09 17:17:09 +0300

    EPMLSTRTAI-309: refactored isInnerRulesValid method

--- src/main/java/com/epam/page/object/generator/validators/AbstractValidator.java
+++ src/main/java/com/epam/page/object/generator/validators/AbstractValidator.java
@@ -7,51 +7,34 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
- * AbstractValidator is the abstract base class for crating a new Validator,
- * which can be used into SearchRuleValidator.
- * <p>
- * For creating a new Validator you need:
- * <ol>
- * <li>Create a new class which extends from {@link AbstractValidator} and override
- * the main method {@link AbstractValidator#isValid(SearchRule, ValidationContext)}.</li>
- * <li>Create default constructor with {@link AbstractValidator#priority}.</li>
- * <li>Override {@link AbstractValidator#getExceptionMessage()}.</li>
- * </ol>
+ * AbstractValidator is the abstract base class for crating a new Validator, which can be used into
+ * SearchRuleValidator. <p> For creating a new Validator you need: <ol> <li>Create a new class which
+ * extends from {@link AbstractValidator} and override the main method {@link
+ * AbstractValidator#isValid(SearchRule, ValidationContext)}.</li> <li>Create default constructor
+ * with {@link AbstractValidator#priority}.</li> <li>Override {@link
+ * AbstractValidator#getExceptionMessage()}.</li> </ol>
  */
 public abstract class AbstractValidator implements Validator {
 
     /**
-     * Validator priority is a number which specifies the order of executing validators.
-     * All validators which present by the default have some default priority, but if you create
-     * your own validator you must not forget to set priority for you validator into default constructor
-     * and change priories for all default validators if you need. <br/><br/>
-     * <p>
-     * For priority you need to use int numbers: <br/>
-     * <ul>
-     * <li>0-49 fot validators which validate format of JSON files</li>
-     * <li>51+ for validators which validate SearchRules by the urls</li>
-     * </ul>
+     * Validator priority is a number which specifies the order of executing validators. All
+     * validators which present by the default have some default priority, but if you create your
+     * own validator you must not forget to set priority for you validator into default constructor
+     * and change priories for all default validators if you need. <br/><br/> <p> For priority you
+     * need to use int numbers: <br/> <ul> <li>0-49 fot validators which validate format of JSON
+     * files</li> <li>51+ for validators which validate SearchRules by the urls</li> </ul>
      *
-     * For example: <br/>
-     * UniquenessLocatorValidator can have priority equals 51,
-     * because it checks that the SearchRule is uniqueness on the WebPage by the url.
-     * It can be like this:<br/>
-     * {@code
-     *  public UniquenessLocatorValidator() {
-     *      super(51);
-     *  }
-     * }
+     * For example: <br/> UniquenessLocatorValidator can have priority equals 51, because it checks
+     * that the SearchRule is uniqueness on the WebPage by the url. It can be like this:<br/> {@code
+     * public UniquenessLocatorValidator() { super(51); } }
      */
     private int priority;
 
     /**
-     * Each validator can work in two modes. <br/>
-     * <ul>
-     * <li>isValidateAllSearchRules = true<br/>
-     * The validator validates all SearchRules every time.</li>
-     * <li>isValidateAllSearchRules = false (default)<br/>
-     * The validator validates only SearchRules that passed the previous validator.</li>
-     * </ul>
+     * Each validator can work in two modes. <br/> <ul> <li>isValidateAllSearchRules = true<br/> The
+     * validator validates all SearchRules every time.</li> <li>isValidateAllSearchRules = false
+     * (default)<br/> The validator validates only SearchRules that passed the previous
+     * validator.</li> </ul>
      */
     private boolean isValidateAllSearchRules = false;
 
@@ -65,15 +48,9 @@ public abstract class AbstractValidator implements Validator {
     }
 
     public boolean isInnerRulesValid(SearchRule searchRule, ValidationContext validationContext) {
-        if (searchRule.getInnerSearchRules() != null) {
-            for (SearchRule innerSearchRule : searchRule.getInnerSearchRules()) {
-                if (!isValid(innerSearchRule, validationContext)) {
-                    return false;
-                }
-            }
-        }
+        return searchRule.getInnerSearchRules() == null || searchRule.getInnerSearchRules().stream()
+            .allMatch(innerSearchRule -> isValid(innerSearchRule, validationContext));
 
-        return true;
     }
 
     @Override
@@ -102,18 +79,16 @@ public abstract class AbstractValidator implements Validator {
     }
 
     /**
-     * This method checks the validity of the {@link SearchRule}.<br/><br/>
-     * <p>
-     * For example (this method check if {@link SearchRule} to have at least one locator: xpath or css):<br/>
-     * {@code
-     *  public boolean isValid(SearchRule searchRule, ValidationContext validationContext) {
-     *      return !isEmpty(searchRule.getCss()) || !isEmpty(searchRule.getXpath());
-     *  }
-     * }
+     * This method checks the validity of the {@link SearchRule}.<br/><br/> <p> For example (this
+     * method check if {@link SearchRule} to have at least one locator: xpath or css):<br/> {@code
+     * public boolean isValid(SearchRule searchRule, ValidationContext validationContext) { return
+     * !isEmpty(searchRule.getCss()) || !isEmpty(searchRule.getXpath()); } }
      *
-     * @param searchRule        searchRule that we validation at the moment.
-     * @param validationContext context for our validators, which contains all {@link ValidationResult} about validation process.
-     * @return <b>true</b> - if {@link SearchRule} is valid<br/> <b>false</b> - if {@link SearchRule} is not valid.
+     * @param searchRule searchRule that we validation at the moment.
+     * @param validationContext context for our validators, which contains all {@link
+     * ValidationResult} about validation process.
+     * @return <b>true</b> - if {@link SearchRule} is valid<br/> <b>false</b> - if {@link
+     * SearchRule} is not valid.
      */
     public abstract boolean isValid(SearchRule searchRule, ValidationContext validationContext);
 
