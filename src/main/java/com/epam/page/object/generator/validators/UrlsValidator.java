22dbaa101e71f4566593a1b52c9635628cf70b0a
konstantin_evstafev <iteaky@gmail.com>
2017-11-22 17:21:04 +0300

    EPMLSTRTAI-337 change urlsValidation. Now it's collect all Exception and the throw them in one.

--- src/main/java/com/epam/page/object/generator/validators/UrlsValidator.java
+++ src/main/java/com/epam/page/object/generator/validators/UrlsValidator.java
@@ -8,13 +8,17 @@ public class UrlsValidator implements Validator {
 
     @Override
     public void validate(ValidationContext validationContext) {
+        StringBuilder invalidUrls = new StringBuilder();
         for (String url : validationContext.getUrls()) {
             try {
                 Jsoup.connect(url).get();
             } catch (IOException | IllegalArgumentException e) {
-                throw new NotValidUrlException("Not valid url: " + url);
+                invalidUrls.append("\n Not valid url: ").append(url);
             }
 
+        }
+        if (invalidUrls.length() > 0){
+            throw new NotValidUrlException(String.valueOf(invalidUrls));
         }
     }
 
